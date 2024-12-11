8a3f54f37aa3ef7f1f9617140b455bd5b972e27c
Andrew Kessler <emailandrewkessler@gmail.com>
2024-12-11 13:46:23 -0500

    Modified Analysis Scripts

--- code/03_analysis.R
+++ code/03_analysis.R
@@ -2,13 +2,16 @@ library(tidyverse)
 library(here)
 library(caret)
 
-### General Data
+### Load Data
 games <- read_csv(here("data", "games.csv"))
 players <- read_csv(here("data", "players.csv"))
 plays <- read_csv(here("data", "plays.csv")) |> 
   mutate(unique_play_id = paste(gameId, playId, sep = "-"))
 decoy_projs <- read_csv("data/decoy_projs.csv")
 test_projs <- read_csv("data/test_projs.csv")
+
+
+################### Model Analysis ###################
 
 ### Confusion Matrix
 conf_data <- test_projs |> 
@@ -33,6 +36,26 @@ bracket_accuracy <- test_projs |>
   summarise(n = n()) |> 
   group_by(odds_bracket) |> 
   mutate(accuracy = n/sum(n))
+
+
+################### Team Analysis ###################
+
+team_decoy_projs <- decoy_projs |> 
+  left_join(select(plays, unique_play_id, possessionTeam),by='unique_play_id')
+
+decoy_rate <- team_decoy_projs |> 
+  group_by(possessionTeam, decoy) |> 
+  summarise(n = n()) |> 
+  group_by(possessionTeam) |> 
+  mutate(rate = n/sum(n)) |> 
+  filter(decoy==1)
+
+avg_model_error <- team_decoy_projs |> 
+  mutate(error = abs(decoy-decoy_odds)) |> 
+  group_by(possessionTeam) |> 
+  summarise(n = n(), total_error = sum(error), avg_error = total_error/n)
+### Do an accuracy test too (just 50/50 cutoff)
+
 
 
 
